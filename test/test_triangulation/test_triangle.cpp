d52371895b03030cb9389cc8473a20cb64e42f14
Thomas Falcone <thomasfalcone.tf@gmail.com>
2023-01-21 00:11:17 +0100

    Add include repositories as additional include folder for both projects Add pathfinding src as additional include directory for the test project Add subfolders in test to order specific tests Add first real unit test
    TODO : Add QPointF to the test folder

---
+++ test/test_triangulation/test_triangle.cpp
@@ -0,0 +1,15 @@
+#include "catch_amalgamated.hpp"
+
+#include "triangulation\triangle2d.h"
+
+TEST_CASE("compare 2 triangles with same points", "[vector]") 
+{
+    QPointF p0 = QPointF(0, 0);
+    QPointF p1 = QPointF(0, 1);
+    QPointF p2 = QPointF(1, 0);
+
+    Triangle2D tri0 = Triangle2D(p0, p1, p2);
+    Triangle2D tri1 = Triangle2D(p1, p2, p0);
+
+    REQUIRE(tri0 == tri1);
+}
