e231b36bfab63f2a373b2c1c1d24e2d2f27852fb
Thomas Falcone <thomasfalcone.tf@gmail.com>
2023-01-21 02:44:31 +0100

    Update test project to be a Qt project compiling QtQuick in order to have native types (QPointF ...)

--- test/test_triangulation/test_triangle.cpp
+++ test/test_triangulation/test_triangle.cpp
@@ -1,5 +1,6 @@
-#include "catch_amalgamated.hpp"
+#include <QPointF>
 
+#include "catch_amalgamated.hpp"
 #include "triangulation\triangle2d.h"
 
 TEST_CASE("compare 2 triangles with same points", "[vector]") 
@@ -8,8 +9,8 @@ TEST_CASE("compare 2 triangles with same points", "[vector]")
     QPointF p1 = QPointF(0, 1);
     QPointF p2 = QPointF(1, 0);
 
-    Triangle2D tri0 = Triangle2D(p0, p1, p2);
-    Triangle2D tri1 = Triangle2D(p1, p2, p0);
+    Triangulation::Triangle2D tri0 = Triangulation::Triangle2D(p0, p1, p2);
+    Triangulation::Triangle2D tri1 = Triangulation::Triangle2D(p1, p2, p0);
 
     REQUIRE(tri0 == tri1);
 }
